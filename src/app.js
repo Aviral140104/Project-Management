6b36b72a2c5438358377b8a728c28b0142ac3626
Aviral <2k23.csaiml2310616@gmail.com>
2025-10-08 15:42:56 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import  cors from 'cors'
+import healthCheckRouter from './routes/healthcheck.routes.js'
+import { healthCheck } from './controllers/healthcheck.controller.js'
 const app = express()
 export default app
 app.get('/', (req, res) => {
@@ -8,7 +10,7 @@ app.get('/', (req, res) => {
 app.use(express.json({limit: '16kb'}));
 app.use(express.urlencoded({ extended: true , limit: '16kb' }));
 app.use(express.static('public'));
-
+app.use("/api/v1/healthcheck",healthCheckRouter)
 app.use(cors({
     origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
     credentials: true,
